3c9e0ccf5fae91eeee16964be90aa80551795fec
A2S-Innovate-Tech <83775048+SankalpGupta2001@users.noreply.github.com>
2024-03-17 22:30:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import Spinner from 'react-bootstrap/Spinner';
-import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import "./App.css";
 
